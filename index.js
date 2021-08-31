e0ff4df123da6f2e469d8e7bddc7c1f40260b101
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-31 12:27:21 +0200

    test 1 to fix cors issue

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
+const cors = require('cors');
 
 require('dotenv').config();
 
 const port = process.env.PORT || 3002;
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(), express.static(path.join(__dirname, 'public')));
 
 const io = require('socket.io')(server, {
 	cors: {
 		origin: process.env.SOCKET_SERVER_REACT_APP,
+		allowedHeaders: ['my-custom-header'],
 	},
 });
 
