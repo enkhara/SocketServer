c226e547a286e4da6665eca30392dd80a88962a1
Georgina Torralba <georgina.torralba@gmail.com>
2021-09-01 08:35:13 +0200

    Fix bug with const app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 //const cors = require('cors');
-const server = require('http').createServer(app);
 const app = express();
+const server = require('http').createServer(app);
 
 require('dotenv').config();
 
